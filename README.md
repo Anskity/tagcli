2f6847cce685fb31c0bde45b24280f4ba6e37347
xijniN <xijnin@gmail.com>
2024-11-03 20:08:43 -0300

    Added readme

---
+++ README.md
@@ -0,0 +1,16 @@
+USAGE
+
+Adds a tag to a file
+```zsh
+tagcli add [tag] [file]
+```
+
+Lists all the files with a specific tag
+```zsh
+tagcli list [tag]
+```
+
+Removes a tag from a file
+```zsh
+tagcli remove [tag] [file]
+```
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = std::env::args().collect();
+    assert!(args.len() > 2);
+
+    let command = &args[1];
+
+    match command.as_str() {
+        "add" => {}
+        "list" => {}
+        "remove" => {}
+        _ => todo!(),
+    }
 }
